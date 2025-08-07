1b2de69ddf6271ba65bdb1354fa259c0c0a3004e
PhoneHomePhone <121206889+PhoneHomePhone@users.noreply.github.com>
2025-08-07 15:45:19 -0500

    Update default.sh
    #ComfyUI-Manager

--- WAN-Video/default.sh
+++ WAN-Video/default.sh
@@ -19,7 +19,7 @@ PIP_PACKAGES=(
 )
 
 NODES=(
-	"https://github.com/ltdrdata/ComfyUI-Manager"
+	#"https://github.com/ltdrdata/ComfyUI-Manager"
     "https://github.com/crystian/ComfyUI-Crystools"
 
 	"https://github.com/cubiq/ComfyUI_essentials"
